705c3f0816bd1881e73daad71610d887d1a2228f
M_Mek <mmekraz781@gmail.com>
2023-11-04 12:27:02 -0400

    created object with dot notation

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
 const topicElement = document.querySelector(".topic");
 const countElement = document.querySelector(".count");
+
+let learning = {};
+learning.category = "Front-End Development";
+learning.topic = "JS";
+learning.topicImportance = "High";
+learning.learningGoals = [
+  "Build JS Programs",
+  "Compile & Retain Knowledge with Obsidian Notes",
+  "Complete my Skillcrush Certificate",
+  "Create Daily Practice Sites",
+];
+console.log(learning);
